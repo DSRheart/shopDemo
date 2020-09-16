8a5f3f1f210e12b8883582ee02e0f9cc43ef06c0
dashanwangluo <514530236@qq.com>
2020-09-16 21:07:49 +0800

    thread

---
+++ pages/classify/classify.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="content">
+		<view class="box1">
+			
+		</view>
+		<view class="box2">
+			
+		</view>
+		<view class="box3">
+			
+		</view>
+		<view class="box4">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content{
+		display: flex;
+		flex-flow: row wrap; 
+	}
+	.box1{
+		background-color: #C0C0C0;
+		width: 33%;
+		height: 100upx;
+	}
+	.box2{
+		background-color: #4CD964;
+		width: 33%;
+		height: 100upx;
+	}
+	.box3{
+		background-color: #333333;
+		width: 33%;
+		height: 100upx;
+	}
+	.box4{
+		background-color: #4CD964;
+		width: 33%;
+		height: 100upx;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,91 @@
+<template>
+	<view>
+		<scroll-view class="content">
+			<view class="home_top_bg">
+				
+			</view>
+		</scroll-view>
+		<!-- 顶部标题 -->
+		<view class="home_top_title_bar">
+			<view class="home_top_title_bar_content">
+				<image src="/static/home_search.png"></image>
+				<view class="home_top_title_bar_text">
+					<text>输入商品名称</text>
+				</view>
+			</view>
+			<view class="home_top_title_bar_message">
+				<image src="/static/home_message.png"></image>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+		
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+	}
+	.home_top_bg{
+		height: 350upx;
+		width: 100%;
+		background-image: linear-gradient(to right, #FF6243 , #D8083A);
+	}
+	.home_top_title_bar{
+		width: 100%;
+		height: 200upx;
+		position: fixed;
+		left: 0;
+		top: 0;
+		display: flex;
+		flex-direction: row;
+		padding-top: var(--status-bar-height);
+		.home_top_title_bar_content{
+			height: 60upx;
+			width: 85%;
+			image{
+				width: 40upx;
+				height: 40upx;
+				margin-left: 20upx;
+				margin-top: 13upx;
+				float: left;
+			}
+			.home_top_title_bar_text{
+				height: 60upx;
+				line-height: 60upx;
+				font-size: 27upx;
+				color: #707070;
+			}
+			background-color: #FFFFFF;
+			margin: 20upx;
+			border-radius: 60upx;
+		}
+		.home_top_title_bar_message{
+			height: 60upx;
+			justify-content: center;
+			image{
+				margin-right: 25upx;
+				margin-top: 20upx;
+				width: 50upx;
+				height:50upx;
+			}
+		}
+	}
+	
+</style>
